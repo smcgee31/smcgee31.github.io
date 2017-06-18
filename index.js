6f41bf1dfbfa08c4808f5c75bea4ad786e2d712d
smcgee31 <solodesign31@gmail.com>
2017-06-18 14:56:47 -0600

    completed card flip functionality

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const config = {
   SECRET_KEY: '1c8abf4fe2fa433581930127172604'
 };
 
-const submit = document.querySelector('#locationInput');
-const weatherDisplay = document.querySelector('#weatherDisplay');
 let coords;
+const submit = document.querySelector('#locationInput');
+const weatherDisplay_f = document.querySelector('#weatherDisplay_f');
+const weatherDisplay_c = document.querySelector('#weatherDisplay_c');
+const results = {
+    city:          ''
+  , state:         ''
+  , updateTime:    ''
+  , url:           ''
+  , condition:     ''
+  , currTemp:      ''
+  , feelsLikeTemp: ''
+  , windDir:       ''
+  , windSpeed:     ''
+}
 
 if(navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(a)  {
@@ -28,24 +40,16 @@ if(navigator.geolocation) {
   }); 
 }
 
-const results = {
-    city:          ''
-  , state:         ''
-  , updateTime:    ''
-  , url:           ''
-  , condition:     ''
-  , currTemp:      ''
-  , feelsLikeTemp: ''
-  , windDir:       ''
-  , windSpeed:     ''
-}
-
 submit.addEventListener('keyup', function(event) {
   event.preventDefault();
   if (event.key === 'Enter') {
     runWeather(coords);
   }
 });
+
+function toggle() {
+  document.querySelector('#flip-toggle').classList.toggle('hover');
+}
 
 function runWeather() {
   let radios = document.querySelectorAll('input[ type = "radio" ]:checked');
@@ -55,12 +59,11 @@ function runWeather() {
     return;
   }
 
-  let scaleCheck = document.querySelector('input[ name = "scaleCheck" ]:checked').value;
   let location = document.getElementById('locationInput').value;
 
   return getCurrentWeather(location || coords)
   .then((response) => {
-    return handleResponse(response, scaleCheck)
+    return mapCurrent(response)
   })
   .then((results) => {
     return displayWeather(results);
@@ -79,51 +82,62 @@ function getCurrentWeather(location) {
   });
 }
 
-function handleResponse(response, scaleCheck) {
-    results.city          = `${response.data.location.name}`;
-    results.state         = `${response.data.location.region}`;
-    results.updateTime    = `${response.data.current.last_updated}`;
-    results.condition     = `${response.data.current.condition.text}`;
-    results.currTemp      = `${response.data.current.temp_f}`;
-    results.feelsLikeTemp = `${response.data.current.feelslike_f}`;
-    results.windDir       = `${response.data.current.wind_dir}`;
-    results.windSpeed     = `${response.data.current.wind_mph} mph`;
-    results.url           = `https:${response.data.current.condition.icon}`;
-    
-  if (scaleCheck === 'celsius') {
-    results.currTemp      = `${response.data.current.temp_c}`;
-    results.feelsLikeTemp = `${response.data.current.feelslike_c}`;
-    results.windSpeed     = `${response.data.current.wind_kph} kph`;
-  }
+function mapCurrent(response) {
+    results.city        = `${response.data.location.name}`;
+    results.state       = `${response.data.location.region}`;
+    results.updateTime  = `${response.data.current.last_updated}`;
+    results.condition   = `${response.data.current.condition.text}`;
+    results.currTemp_f  = `${response.data.current.temp_f}`;
+    results.currTemp_c  = `${response.data.current.temp_c}`;
+    results.feelslike_f = `${response.data.current.feelslike_f}`;
+    results.feelslike_c = `${response.data.current.feelslike_c}`;
+    results.windDir     = `${response.data.current.wind_dir}`;
+    results.wind_mph    = `${response.data.current.wind_mph}`;
+    results.wind_kph    = `${response.data.current.wind_kph}`;
+    results.url         = `https:${response.data.current.condition.icon}`;
 
   return results;
 }
 
-function handleError(response) {
-  console.error('ERROR:', response);
-}
-
 function displayWeather(results) {
-  function html() {
-    return `
-      <div class="card card-2">
-        <div id="location">
-          <p class="cityState">${results.city}, ${results.state}</p>
-          <p class="updateTime">Last updated: ${results.updateTime}</p>
-        </div>
-        <div id="temp">
-          <img src="${results.url}" alt="weather_image">
-          <p class="generalCondition">${results.condition}</p>
-          <p class="currTemp">${results.currTemp}<span class="degMarker">&deg;F</span></p>
-          <p class="feelsLikeTemp">(feels like ${results.feelsLikeTemp}&deg;)</p>
-        </div>
-        <div id="wind">
-          <p class="windDir">Winds from the ${results.windDir}</p>
-          <p class="windSpeed">at ${results.windSpeed}</p>
-        </div>
-      </div>
-    `;
-  }
 
-  return weatherDisplay.innerHTML = html();
+  weatherDisplay_f.innerHTML = `
+    <div class="card">
+      <div class="location">
+        <p class="cityState">${results.city}, ${results.state}</p>
+        <p class="updateTime">Last updated: ${results.updateTime}</p>
+      </div>
+      <div class="temp">
+        <img src="${results.url}" alt="weather_image">
+        <p class="generalCondition">${results.condition}</p>
+        <p class="currTemp">${results.currTemp_f}<span class="degMarker">&deg;F</span></p>
+        <p class="feelsLikeTemp">(feels like ${results.feelslike_f}&deg;)</p>
+      </div>
+      <div class="wind">
+        <p class="windDir">Winds from the ${results.windDir}</p>
+        <p class="windSpeed">at ${results.wind_mph} mph</p>
+      </div>
+    </div>
+  `;
+
+  weatherDisplay_c.innerHTML = `
+    <div class="card">
+      <div class="location">
+        <p class="cityState">${results.city}, ${results.state}</p>
+        <p class="updateTime">Last updated: ${results.updateTime}</p>
+      </div>
+      <div class="temp">
+        <img src="${results.url}" alt="weather_image">
+        <p class="generalCondition">${results.condition}</p>
+        <p class="currTemp">${results.currTemp_c}<span class="degMarker">&deg;C</span></p>
+        <p class="feelsLikeTemp">(feels like ${results.feelslike_c}&deg;)</p>
+      </div>
+      <div class="wind">
+        <p class="windDir">Winds from the ${results.windDir}</p>
+        <p class="windSpeed">at ${results.wind_kph} kph</p>
+      </div>
+    </div>
+  `;
+
+  return results;
 }
